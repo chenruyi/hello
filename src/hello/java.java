253af7cb3391edbd0cbd63aaedf478739add5047
chen <chenruyi0627@outlook.com>
2018-10-30 22:31:22 +0800

    Update the java.java

--- src/hello/java.java
+++ src/hello/java.java
@@ -3,7 +3,6 @@ package hello;
 public class java {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		System.out.println("hello world!!----twice");
 	}
